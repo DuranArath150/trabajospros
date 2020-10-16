4816fc1bb689fd32b1b2faf7333f7cd16d4ed1f5
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2020-10-16 13:49:48 -0700

    Update 53.cpp

--- 53.cpp
+++ 53.cpp
@@ -1,3 +1,17 @@
+  
+/* @@
+    @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Arath Duran Hernandez @nickname DuranArath150
+    @@ Repositorio: http://github.com/DuranArath/trabajospros
+    @@ Fecha de revisión: ____
+    @@ 
+    @
+    @ Objetivo del programa:
+    @  Sumas de numeros primos dependiendo que tantos numeros primos se quieran*/
+
 #include <iostream>
 
 using namespace std;
